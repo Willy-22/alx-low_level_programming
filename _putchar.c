9f87757973df56203b381ae2af44d728cf34dd07
Edeh chinemerem <willamsedeh@gmail.com>
2023-06-23 09:55:17 -0700

    first

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+  * _putchar - print out character to stdout
+  * @d: the character to print out
+  * Return: 1 (success)
+  */
+
+int _putchar(char d)
+{
+	return write(1, &d, 1);
+}
