24e0cc5ec61876b47f3458f2bdd49f6d467d8a23
Edeh chinemerem <willamsedeh@gmail.com>
2023-06-23 12:59:45 -0700

    Number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - print number 0 t0 9
+  * Return: 0 success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
