a3a69137e9b0d1f3284a5bc5e600309a184b61ba
Edeh chinemerem <willamsedeh@gmail.com>
2023-06-23 12:15:32 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - multiple two numbers
+  * @a: variable
+  * @b: variable
+  * Return: 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int mu;
+
+	mu = a * b;
+	return (mu);
+}
