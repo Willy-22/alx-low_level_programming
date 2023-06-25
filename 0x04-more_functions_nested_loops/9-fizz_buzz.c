620f6a0e8d7707c70c09655f2f90276e92429cb5
Edeh chinemerem <willamsedeh@gmail.com>
2023-06-25 11:31:39 -0700

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" fizzbuzz");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf(" fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf(" buzz");
+			printf(" Buzz");
 		}
 		else
 		{
