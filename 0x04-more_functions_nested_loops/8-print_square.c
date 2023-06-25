c218d0aeae560a4283ca55162d029d53d235dd16
Edeh chinemerem <willamsedeh@gmail.com>
2023-06-25 09:19:50 -0700

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_square - prints square on the terminal using #
+  * @size of the square
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int j, i;
+
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
