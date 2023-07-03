e91aa55406a2ae5d7c06464c273dd18d9bc5faa0
Edeh chinemerem <willamsedeh@gmail.com>
2023-07-02 18:33:21 -0700

    Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - Encodes a string using the ROT13 cipher.
+ * @s: Pointer to the string to be encoded.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
