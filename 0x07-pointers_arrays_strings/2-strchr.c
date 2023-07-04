3d765a936dd806d1fc562414ee270e9ecbc551c0
Edeh chinemerem <willamsedeh@gmail.com>
2023-07-04 12:41:33 -0700

    Return character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,14 @@
 
 char *_strchr(char *s, char c)
 {
-	for (; *s != '\0'; s++)
+	int i;
+
+	for (i = 0; s[i]  >= '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
 	}
-	return ('\0');
+	return (NULL);
 }
